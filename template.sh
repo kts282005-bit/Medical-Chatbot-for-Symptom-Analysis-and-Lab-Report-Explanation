fda40eae0981d002d142e491d52432aebab9e9f4
kts282005-bit <kts282005@gmail.com>
2026-03-04 12:32:22 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+# Creating directory
+mkdir -p src
+mkdir -p research
+
+#Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch research/trails.txt
+touch requirements.txt
+
+echo "Directory and files created successfully!."
