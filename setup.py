45eaf53a4e6622cf87c0263edd0b5784ddfd30ef
kts282005-bit <kts282005@gmail.com>
2026-03-05 12:37:30 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup 
+
+setup(
+    name="medical_chatbot"
+    version="0.1.0"
+    author="Sriram K.T"
+    author_email="kts282005@gmail.com"
+    packages=find_packages(),
+    install_requires=[]
+)
